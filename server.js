b1bd1b60630e7668e8de5b7a8c9ccb0c46614918
unknown <sherman.jonathan.r@gmail.com>
2019-05-13 18:18:06 -0700

    route change

--- server.js
+++ server.js
@@ -6,8 +6,8 @@ let PORT = process.env.PORT || 3000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-require('./routing/apiRoutes')(app);
-require('./routing/htmlRoutes')(app);
+require('app/routing/apiRoutes')(app);
+require('app/routing/htmlRoutes')(app);
 
 app.listen(PORT, () => {
     console.log('App is listening on localhost: ' + 3000);
